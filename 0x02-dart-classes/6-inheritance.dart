305eacfbfa49c58996289fbb4277f099070d7217
LucileSD <4316@holbertonschool.com>
2023-07-20 14:56:07 +0200

    ok not pxd in fromjson

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -4,16 +4,16 @@ class User extends Password {
   String name;
   int age;
   double height;
-  int id;
-  String user_password;
+  int? id;
+  String? user_password;
 
   User({
     required this.name,
     required this.age,
     required this.height,
-    required this.id,
-    required this.user_password,
-  }) : super(password: user_password);
+    int? this.id,
+    String? this.user_password,
+  });
 
   Map<String, dynamic> toJson() {
     return {
@@ -30,16 +30,12 @@ class User extends Password {
       name: userJson['name'],
       age: userJson['age'],
       height: userJson['height'],
-      user_password: userJson['user_password'] ?? '',
     );
-  }
-
-  bool isPasswordValid() {
-    return Password(password: user_password).isValid();
   }
 
   @override
   String toString() {
-    return 'User(id : ${id} ,name: ${name}, age: ${age}, height: ${height}, Password: ${isPasswordValid()})';
+    final pwd = Password(password: user_password ?? '');
+    return 'User(id : ${id} ,name: ${name}, age: ${age}, height: ${height}, Password: ${pwd.isValid()})';
   }
 }
