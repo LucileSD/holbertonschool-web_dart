73a368d2635d5aa0a73f0d3187a6524a88ff7191
LucileSD <4316@holbertonschool.com>
2023-07-24 09:49:46 +0200

    read and correction

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
-
 void main(List<String> args) {
-	var nb = int.parse(args[0]);
-	assert(nb >= 80, '\"The score must be bigger or equal to 80');
-	print('You Passed');
+  var nb = int.parse(args[0]);
+  assert(nb >= 80, '\"The score must be bigger or equal to 80');
+  print('You Passed');
 }
---
+++ 0x02-dart-classes/README.md
@@ -0,0 +1,3 @@
+## 0x02. Dart - Classes
+
+Learn how use classes in Dart
